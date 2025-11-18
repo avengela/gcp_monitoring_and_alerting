9ce21426928b9826848e567bad2e7a93890686ea
Miyukivv <natalia.kielarr@interia.pl>
2025-11-18 15:08:03 +0100

    added variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,33 @@
+variable "project_id" {
+    description = "Google Cloud project ID"
+    type = string
+    default = "your-project-Id"
+}
+
+variable "region"{
+    description = "GCE region"
+    type = string
+    default = "your-region"
+}
+
+variable "machine_type"{
+    description = "GCE machine type"
+    type = string
+    default = "your-vm-machine-type"
+}
+
+variable "zone"{
+    description ="GCE zone"
+    type =string
+    default = "your-vm-zone"
+}
+
+variable "project_number"{
+    type = string
+    default = "your-project-number"
+}
+
+variable "email_name"{
+    type = string
+    default = "your-email"
+}
